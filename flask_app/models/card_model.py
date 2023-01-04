a2887f4ca8286238841522c197a98025d3fefe6f
Robert Lutrick <r.lutrick@icloud.com>
2023-01-04 12:58:22 -0600

    Syntax update

--- flask_app/models/card_model.py
+++ flask_app/models/card_model.py
@@ -1,15 +1,14 @@
 class Card:
 
-    def __init__(self, suit, point_val, string_val):
+    def __init__(self, suit, string_val, point_val):
 
         self.suit = suit
+        self.string_val = string_val
 
         if point_val > 10 and string_val != "Ace":
             self.point_val = 10
         else:
             self.point_val = point_val
 
-        self.string_val = string_val
-
     def card_info(self):
-        print(f"{self.string_val} {self.suit}")
+        print(f"{self.string_val}_of_{self.suit}")
